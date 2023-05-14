807acc4e3a22b67b92e88e876669d4e80b9db770
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-05-14 18:34:48 +0200

    made fileioService more readable

--- fileIO/src/main/scala/restFileIO/FileIOService.scala
+++ fileIO/src/main/scala/restFileIO/FileIOService.scala
@@ -18,47 +18,45 @@ import scala.util.{Failure, Success}
 import akka.protobufv3.internal.compiler.PluginProtos.CodeGeneratorResponse.File
 import play.api.libs.json.*
 
-class FileIOService():
+class FileIOService() {
     implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
     implicit val executionContext: ExecutionContextExecutor = system.executionContext
-
     val fileIO = FileIO()
     val RestUIPort = 8082
     val routes: String =
         """
             """.stripMargin
-
     val route: Route =
         concat(
-        pathSingleSlash {
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, routes))
-        },
-        get {
-            path("load") {
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, fileIO.gameToJson(fileIO.load).toString()))
-            }
-        },
-        put {
-            path("save") {
-            entity(as[String]) { data =>
-                complete {
-                fileIO.save(fileIO.jsonToGame(data))
-                Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, "game successfully saved"))
+            pathSingleSlash {
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, routes))
+            },
+            get {
+                path("load") {
+                    complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, fileIO.gameToJson(fileIO.load).toString()))
+                }
+            },
+            put {
+                path("save") {
+                    entity(as[String]) { data =>
+                        complete {
+                            fileIO.save(fileIO.jsonToGame(data))
+                            Future.successful(HttpEntity(ContentTypes.`text/html(UTF-8)`, "game successfully saved"))
+                        }
+                    }
                 }
             }
-            }
-        }
         )
 
     def start(): Unit = {
         val binding = Http().newServerAt("localhost", RestUIPort).bind(route)
-
         binding.onComplete {
-        case Success(binding) => {
-            println(s"UNO PersistenceAPI service online at http://localhost:$RestUIPort/")
+            case Success(binding) => {
+                println(s"Romme FileIO at http://localhost:$RestUIPort/")
+            }
+            case Failure(exception) => {
+                println(s"Romme FileIO failed to start: ${exception.getMessage}")
+            }
         }
-        case Failure(exception) => {
-            println(s"UNO PersistenceAPI service failed to start: ${exception.getMessage}")
-        }
-        }
-  }
+    }
+}
